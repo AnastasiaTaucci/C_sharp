233ca4418ee8cbc26d8291959ba482b6dbd92fd7
Anastasia Taucci <112522011+AnastasiaTaucci@users.noreply.github.com>
2025-01-15 18:22:31 -0800

    Add files via upload

---
+++ Students_HW_week1/Students_HW_week1/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Students_HW_week1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //getting students' names
+            Console.WriteLine("please enter first student's First Name: ");
+            string FirstName1 = Console.ReadLine();
+            Console.WriteLine("please enter first student's Last Name: ");
+            string LastName1 = Console.ReadLine();
+
+
+            Console.WriteLine("please enter second student's First Name: ");
+            string FirstName2 = Console.ReadLine();
+            Console.WriteLine("please enter second student's Last Name: ");
+            string LastName2 = Console.ReadLine();
+
+
+            Console.WriteLine("please enter third student's First Name: ");
+            string FirstName3 = Console.ReadLine();
+            Console.WriteLine("please enter third student's Last Name: ");
+            string LastName3 = Console.ReadLine();
+
+
+            //creating objects
+            Student[] Students = new Student[3] {new Student(FirstName1, LastName1), new Student(FirstName2, LastName2), new Student(FirstName3, LastName3)};
+
+            foreach (Student student in Students)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"My name is {student.FirstName} {student.LastName} and my average is {student.GradeAverage()}");
+                Console.WriteLine();
+            }
+            
+            Console.ReadLine();
+        }
+    }
+
+
+
+}
---
+++ Students_HW_week1/Students_HW_week1/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Students_HW_week1
+{
+    public class Student
+    {
+        //constructors
+        public Student(string pFirstName, string pLastName) 
+        {
+            FirstName = pFirstName;
+            LastName = pLastName;
+        }
+
+        //properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //fields
+        private double[] grades = new double[4] {9, 10, 8, 10};
+
+        //methods
+        public double GradeAverage()
+        {
+            double sum_notes = 0;
+            foreach (double grade in grades)
+            {
+                sum_notes += grade;
+            }
+            //Console.WriteLine(grades.Length);
+            //Console.WriteLine(sum_notes / grades.Length);
+            return sum_notes/grades.Length;            
+
+        }
+    }
+}
