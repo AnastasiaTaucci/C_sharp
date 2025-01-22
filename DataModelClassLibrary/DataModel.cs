2a430e6a4d3978bb21dcbf02ba926a1332c551c3
Anastasia Taucci <112522011+AnastasiaTaucci@users.noreply.github.com>
2025-01-21 23:07:52 -0800

    Add files via upload

---
+++ DataModelClassLibrary/DataModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DataModelClassLibrary
+{
+    public class DataModel
+    {
+        private static string[,] fakeDB;
+
+        public static bool SaveNewPermit(string userName, string zip)
+        {
+            fakeDB = DiskStore.ReadStringArray();
+            for (int i = 0; i < fakeDB.GetLength(0); i++)
+            {
+                if (fakeDB[i, 0] == null || fakeDB[i,0] == "")
+                {
+                    fakeDB[i, 0] = userName;
+                    fakeDB[i, 1] = zip;
+                    fakeDB[i, 2] = DateTime.Now.ToShortDateString();
+                    DiskStore.WriteStringArray(fakeDB);
+                    return true; // success
+                }
+
+            }
+            return false; //  return false if full
+        }
+
+        public static string[,] GetPermitsDM()
+        {
+           return DiskStore.ReadStringArray();
+        }
+    }
+}
---
+++ DataModelClassLibrary/DiskStore.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DataModelClassLibrary
+{
+    internal static class DiskStore
+    {
+        static string currentDocPath =
+        Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) +
+        "\\arrayData.txt";
+
+        public static void WriteStringArray(string[,] pArrayIn)
+        {
+            StreamWriter fileWriter = new StreamWriter(currentDocPath);
+
+            for (int i = 0; i < pArrayIn.GetLength(0); i++) 
+            {
+                for (int j = 0; j < pArrayIn.GetLength(1); j++)
+                {
+                    fileWriter.WriteLine(pArrayIn[i, j]);
+                }
+            }
+            fileWriter.Close();
+        }
+
+
+        public static string[,] ReadStringArray()
+        {
+            VerifyFileExists(10, 3);
+
+            string [,] readArray = new string[10, 3];
+
+            StreamReader fileReader = new StreamReader(currentDocPath);
+
+            for (int i = 0; i <  readArray.GetLength(0); i++)
+            {
+                for(int j = 0;j < readArray.GetLength(1);j++)
+                {
+                    readArray[i, j] = fileReader.ReadLine();
+                }
+            }
+
+            fileReader.Close();
+            return readArray;
+            
+        }
+
+
+        public static bool VerifyFileExists(int rows, int columns)
+        {
+            try
+            {
+                if (File.Exists(currentDocPath))
+                {
+                    return true;
+                }
+                else
+                {
+                    CreateFile(rows, columns);
+                    return true;
+                }
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine("We have a problem: " + ex.Message);
+            }
+            return false;
+        }
+
+
+
+
+        public static void CreateFile(int rows, int columns)
+        {
+            StreamWriter arrayCreator = new StreamWriter(currentDocPath);
+            for (int i = 0; i < rows; i++)
+            {
+                arrayCreator.WriteLine("");
+            }
+            arrayCreator.Close();
+        }
+    }
+}
